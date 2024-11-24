4f2893a9a8f46b45e1bf7ebdbfcefefaf2bc4bc6
kanisha williams <64101937+kanishawilliams@users.noreply.github.com>
2024-11-23 20:38:03 -0600

    Update and rename 16-4.cpp to GroupWhite_16-4.cpp

--- GroupWhite_16-4.cpp
+++ GroupWhite_16-4.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+Instructor: Prof. Charlesvester Wims
+Course: CS 109
+Assignment: Module 6-7 Lab (Final) -- Chapter 16, Problem 4
+Date: November 23, 2024
+Group White (Bruce D., Mikaela P., Savalas P., Kanisha W.)
+*******************************************************************************/
+
 #include <iostream>
 #include <string>
 #include <cctype>
